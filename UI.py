99af81c7c8fc78afac5a2728c161c21081d78c84
amf5214 <81787764+amf5214@users.noreply.github.com>
2021-04-20 10:00:36 -0400

    new interface and added power, clear, clear all buttons

--- UI.py
+++ UI.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow, QFrame, QGridLayout
+from PyQt5.QtGui import QIcon
 from funcs import *
 import logging as lg
 
@@ -6,6 +7,8 @@ lg.basicConfig(filename='calc.log',
                level=lg.INFO,
                format='%(levelname)s : %(asctime)s : %(message)s',
                filemode='w')
+
+final = ''
 
 
 class Calc(QMainWindow):
@@ -15,16 +18,18 @@ class Calc(QMainWindow):
         self.root = QFrame(self)
         self.setCentralWidget(self.root)
         self.lay_main = QGridLayout(self.root)
+        self.setWindowTitle('Calculator')
+        self.setWindowIcon(QIcon('calc_icon.jpg'))
+        self.setStyleSheet('background-color: white')
+        # self.setWindowFlags(Qt.FramelessWindowHint)
 
         create_buttons(self)
-
         self.widgets_add()
-        update_screen(self)
+        self.update_screen('Basic Calculator')
 
         self.show()
 
     def widgets_add(self):
-
         self.lay_main.addWidget(self.output_screen, 0, 0, 2, 4)
         self.lay_main.addWidget(self.n7, 3, 0)
         self.lay_main.addWidget(self.n8, 3, 1)
@@ -40,5 +45,211 @@ class Calc(QMainWindow):
         self.lay_main.addWidget(self.subtract, 4, 3)
         self.lay_main.addWidget(self.multiply, 5, 3)
         self.lay_main.addWidget(self.divide, 6, 3)
+        self.lay_main.addWidget(self.power, 6, 2)
         self.lay_main.addWidget(self.enter, 7, 3)
+        self.lay_main.addWidget(self.clear, 7, 0)
+        self.lay_main.addWidget(self.all_clear, 7, 1)
+
+    def update_screen(self, obj):
+        self.output_screen.setText(obj)
+
+    def push_0(self):
+        number = 0
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_1(self):
+        number = 1
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_2(self):
+        number = 2
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_3(self):
+        number = 3
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_4(self):
+        number = 4
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_5(self):
+        number = 5
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_6(self):
+        number = 6
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_7(self):
+        number = 7
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_8(self):
+        number = 8
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_9(self):
+        number = 9
+        state = 'number'
+        self.output_modify(number, state)
+
+    def push_add(self):
+        number = ' + '
+        state = 'symbol'
+        self.output_modify(number, state)
+
+    def push_subtract(self):
+        number = ' - '
+        state = 'symbol'
+        self.output_modify(number, state)
+
+    def push_multiply(self):
+        number = ' * '
+        state = 'symbol'
+        self.output_modify(number, state)
+
+    def push_divide(self):
+        number = ' / '
+        state = 'symbol'
+        self.output_modify(number, state)
+
+    def push_power(self):
+        number = ' ^ '
+        state = 'symbol'
+        self.output_modify(number, state)
+
+    def output_modify(self, number, state):
+        global final, output
+        if state == 'number':
+            final += str(number)
+        else:
+            final += number
+
+        output = final.split(sep=' ')
+        self.update_screen(f'{final}')
+        # print(final)
+        # print(output)
+
+    def add_func(self):
+        global output
+        output = np.array(output)
+
+        ind = np.where(output == '+')
+        ind = ind[0]
+        # print(ind)
+
+        for x in ind:
+            if len(output) > x + 1:
+                result_out = str(float(output[x - 1]) + float(output[x + 1]))
+                output = np.delete(output, x - 1)
+                output = np.delete(output, x - 1)
+
+                output = list(output)
+                output[x - 1] = result_out
+
+    def subtract_func(self):
+        global output
+        output = np.array(output)
+
+        ind = np.where(output == '-')
+        ind = ind[0]
+        # print(ind)
+
+        for x in ind:
+            if len(output) > x + 1:
+                result_out = str(float(output[x - 1]) - float(output[x + 1]))
+                output = np.delete(output, x - 1)
+                output = np.delete(output, x - 1)
+
+                output = list(output)
+                output[x - 1] = result_out
+
+    def multiply_func(self):
+        global output
+        output = np.array(output)
+
+        ind = np.where(output == '*')
+        ind = ind[0]
+        # print(ind)
+
+        for x in ind:
+            if len(output) > x + 1:
+                result_out = str(float(output[x - 1]) * float(output[x + 1]))
+                output = np.delete(output, x - 1)
+                output = np.delete(output, x - 1)
+
+                output = list(output)
+                output[x - 1] = result_out
+
+    def divide_func(self):
+        global output
+        output = np.array(output)
+
+        ind = np.where(output == '/')
+        ind = ind[0]
+        # print(ind)
+
+        for x in ind:
+            if len(output) > x + 1:
+                result_out = str(float(output[x - 1]) / float(output[x + 1]))
+                output = np.delete(output, x - 1)
+                output = np.delete(output, x - 1)
+
+                output = list(output)
+                output[x - 1] = result_out
+
+    def power_func(self):
+        global output
+        output = np.array(output)
+
+        ind = np.where(output == '^')
+        ind = ind[0]
+        # print(ind)
+
+        for x in ind:
+            if len(output) > x + 1:
+                result_out = str(float(output[x - 1]) ** float(output[x + 1]))
+                output = np.delete(output, x - 1)
+                output = np.delete(output, x - 1)
+
+                output = list(output)
+                output[x - 1] = result_out
+
+    def push_enter(self):
+        global output, final
+        while '^' in output:
+            self.power_func()
+
+        while '*' in output:
+            self.multiply_func()
+
+        while '/' in output:
+            self.divide_func()
+
+        while '+' in output:
+            self.add_func()
+
+        while '-' in output:
+            self.subtract_func()
+
+        # print(str(output[0]))
+        self.update_screen(f'{final} = {str(output[0])}')
+
+    def push_all_clear(self):
+        global final
+        self.update_screen('')
+        final = ''
+
+    def push_clear(self):
+        self.update_screen(f'{final}')
 
--- funcs.py
+++ funcs.py
@@ -1,266 +1,103 @@
 from PyQt5.QtWidgets import QPushButton, QLabel
 from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
 import numpy as np
-
-final = ''
-output = []
 
 
 def create_buttons(self):
     self.n0 = QPushButton('0')
     self.n0.setFont(QFont('Times', 15))
-    self.n0.clicked.connect(push_0)
+    self.n0.clicked.connect(self.push_0)
+    self.n0.setStyleSheet('background-color: black;''color: white')
 
     self.n1 = QPushButton('1')
     self.n1.setFont(QFont('Times', 15))
-    self.n1.clicked.connect(push_1)
+    self.n1.clicked.connect(self.push_1)
+    self.n1.setStyleSheet('background-color: black;''color: white')
 
     self.n2 = QPushButton('2')
     self.n2.setFont(QFont('Times', 15))
-    self.n2.clicked.connect(push_2)
+    self.n2.clicked.connect(self.push_2)
+    self.n2.setStyleSheet('background-color: black;''color: white')
 
     self.n3 = QPushButton('3')
     self.n3.setFont(QFont('Times', 15))
-    self.n3.clicked.connect(push_3)
+    self.n3.clicked.connect(self.push_3)
+    self.n3.setStyleSheet('background-color: black;''color: white')
 
     self.n4 = QPushButton('4')
     self.n4.setFont(QFont('Times', 15))
-    self.n4.clicked.connect(push_4)
+    self.n4.clicked.connect(self.push_4)
+    self.n4.setStyleSheet('background-color: black;''color: white')
 
     self.n5 = QPushButton('5')
     self.n5.setFont(QFont('Times', 15))
-    self.n5.clicked.connect(push_5)
+    self.n5.clicked.connect(self.push_5)
+    self.n5.setStyleSheet('background-color: black;''color: white')
 
     self.n6 = QPushButton('6')
     self.n6.setFont(QFont('Times', 15))
-    self.n6.clicked.connect(push_6)
+    self.n6.clicked.connect(self.push_6)
+    self.n6.setStyleSheet('background-color: black;''color: white')
 
     self.n7 = QPushButton('7')
     self.n7.setFont(QFont('Times', 15))
-    self.n7.clicked.connect(push_7)
+    self.n7.clicked.connect(self.push_7)
+    self.n7.setStyleSheet('background-color: black;''color: white')
 
     self.n8 = QPushButton('8')
     self.n8.setFont(QFont('Times', 15))
-    self.n8.clicked.connect(push_8)
+    self.n8.clicked.connect(self.push_8)
+    self.n8.setStyleSheet('background-color: black;''color: white')
 
     self.n9 = QPushButton('9')
     self.n9.setFont(QFont('Times', 15))
-    self.n9.clicked.connect(push_9)
+    self.n9.clicked.connect(self.push_9)
+    self.n9.setStyleSheet('background-color: black;''color: white')
 
     self.add = QPushButton('+')
     self.add.setFont(QFont('Times', 15))
-    self.add.clicked.connect(push_add)
+    self.add.clicked.connect(self.push_add)
+    self.add.setStyleSheet('background-color: black;''color: white')
 
     self.subtract = QPushButton('-')
     self.subtract.setFont(QFont('Times', 15))
-    self.subtract.clicked.connect(push_subtract)
+    self.subtract.clicked.connect(self.push_subtract)
+    self.subtract.setStyleSheet('background-color: black;''color: white')
 
     self.multiply = QPushButton('*')
     self.multiply.setFont(QFont('Times', 15))
-    self.multiply.clicked.connect(push_multiply)
+    self.multiply.clicked.connect(self.push_multiply)
+    self.multiply.setStyleSheet('background-color: black;''color: white')
 
     self.divide = QPushButton('/')
     self.divide.setFont(QFont('Times', 15))
-    self.divide.clicked.connect(push_divide)
+    self.divide.clicked.connect(self.push_divide)
+    self.divide.setStyleSheet('background-color: black;''color: white')
+
+    self.power = QPushButton('^')
+    self.power.setFont(QFont('Times', 15))
+    self.power.clicked.connect(self.push_power)
+    self.power.setStyleSheet('background-color: black;''color: white')
 
     self.enter = QPushButton('Enter')
     self.enter.setFont(QFont('Times', 15))
-    self.enter.clicked.connect(push_enter)
+    self.enter.clicked.connect(self.push_enter)
+    self.enter.setStyleSheet('background-color: black;''color: white')
 
     self.output_screen = QLabel()
     self.output_screen.setFont(QFont('Times', 25))
+    self.output_screen.setAlignment(Qt.AlignCenter)
+    self.output_screen.setStyleSheet('border: 5px solid black;''background-color: white;''color: black')
 
+    self.clear = QPushButton('Clear')
+    self.clear.setFont(QFont('Times', 15))
+    self.clear.clicked.connect(self.push_clear)
+    self.clear.setStyleSheet('background-color: black;''color: white')
 
-def push_0(self):
-    number = 0
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_1(self):
-    number = 1
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_2(self):
-    number = 2
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_3(self):
-    number = 3
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_4(self):
-    number = 4
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_5(self):
-    number = 5
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_6(self):
-    number = 6
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_7(self):
-    number = 7
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_8(self):
-    number = 8
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_9(self):
-    number = 9
-    state = 'number'
-    output_modify(self, number, state)
-
-
-def push_add(self):
-    number = ' + '
-    state = 'symbol'
-    output_modify(self, number, state)
-
-
-def push_subtract(self):
-    number = ' - '
-    state = 'symbol'
-    output_modify(self, number, state)
-
-
-def push_multiply(self):
-    number = ' * '
-    state = 'symbol'
-    output_modify(self, number, state)
-
-
-def push_divide(self):
-    number = ' / '
-    state = 'symbol'
-    output_modify(self, number, state)
-
-
-def output_modify(self, number, state):
-    global final, output
-    if state == 'number':
-        final += str(number)
-    else:
-        final += number
-
-    output = final.split(sep=' ')
-    print(final)
-    print(output)
-
-
-def add_func(self):
-    global output
-    output = np.array(output)
-
-    ind = np.where(output == '+')
-    ind = ind[0]
-    print(ind)
-
-    for x in ind:
-        if len(output) > x + 1:
-            result_out = str(float(output[x - 1]) + float(output[x + 1]))
-            output = np.delete(output, x - 1)
-            output = np.delete(output, x - 1)
-
-            output = list(output)
-            output[x - 1] = result_out
-
-
-def subtract_func(self):
-    global output
-    output = np.array(output)
-
-    ind = np.where(output == '-')
-    ind = ind[0]
-    print(ind)
-
-    for x in ind:
-        if len(output) > x + 1:
-            result_out = str(float(output[x - 1]) - float(output[x + 1]))
-            output = np.delete(output, x - 1)
-            output = np.delete(output, x - 1)
-
-            output = list(output)
-            output[x - 1] = result_out
-
-
-def multiply_func(self):
-    global output
-    output = np.array(output)
-
-    ind = np.where(output == '*')
-    ind = ind[0]
-    print(ind)
-
-    for x in ind:
-        if len(output) > x + 1:
-            result_out = str(float(output[x - 1]) * float(output[x + 1]))
-            output = np.delete(output, x - 1)
-            output = np.delete(output, x - 1)
-
-            output = list(output)
-            output[x - 1] = result_out
-
-
-def divide_func(self):
-    global output
-    output = np.array(output)
-
-    ind = np.where(output == '/')
-    ind = ind[0]
-    print(ind)
-
-    for x in ind:
-        if len(output) > x + 1:
-            result_out = str(float(output[x - 1]) / float(output[x + 1]))
-            output = np.delete(output, x - 1)
-            output = np.delete(output, x - 1)
-
-            output = list(output)
-            output[x - 1] = result_out
-
-
-def push_enter(self):
-    global output
-    while '*' in output:
-        multiply_func(self)
-
-    while '/' in output:
-        divide_func(self)
-
-    while '+' in output:
-        add_func(self)
-
-    while '-' in output:
-        subtract_func(self)
-
-
-def update_screen(self):
-    global output
-    text = ''
-    for x in output:
-        text += str(x)
-    self.output_screen.setText(text)
-
-
+    self.all_clear = QPushButton('All Clear')
+    self.all_clear.setFont(QFont('Times', 15))
+    self.all_clear.clicked.connect(self.push_all_clear)
+    self.all_clear.setStyleSheet('background-color: black;''color: white')
 
 
